168cc5a537b62d0aae9c15a5a9ab29a941fc0849
Zilkowski, Joseph <jrzilko@ilstu.edu>
2019-04-04 21:06:13 -0500

    Start of project with main file

---
+++ AlarmClock/src/alarmClock/main.java
@@ -0,0 +1,13 @@
+package alarmClock;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		//This is the beginning of the alarm clock program.
+		//The base idea of this main method is just to activate the alarm clock GUI then end the program.
+		
+		
+	}
+
+}
